d6b66e28eda1a69f9477588098f8e9d5dc865db1
Onan Thairu <thairuonan@gmail.com>
2023-10-26 19:52:19 +0300

    chore: style sponsors section.

--- src/main.js
+++ src/main.js
@@ -3,3 +3,4 @@ import '../styles/components/header.css';
 import '../styles/components/hero.css';
 import '../styles/components/discover.css';
 import '../styles/components/tour.css';
+import '../styles/components/sponsors.css'
